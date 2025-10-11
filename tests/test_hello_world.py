f9a971eb89841ea842a65ecce44c9757e8dea3e9
Ezekiel Bassey <bazzdude007@gmail.com>
2025-10-11 09:08:06 +0100

    Create test_hello_world.py to test hello_world.py

---
+++ tests/test_hello_world.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+import io
+from contextlib import redirect_stdout
+
+
+class TestHelloWorld(TestCase):
+
+  def test_hello_world(self):
+    captured_output = io.StringIO()
+    with open("foundations/hello_world.py") as f:
+      with redirect_stdout(captured_output):
+        exec(f.read())
+    self.assertEqual(captured_output.getvalue().strip(), "Hello World!")
+
+if __name__ == "__main__":
+  unittest.main()
